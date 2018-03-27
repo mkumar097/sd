e976345a5f1fd844a355ba5b7f9a14a9097df7b9
Manav Kumar <kumar@dhcp-wifi-8021x-155-41-98-108.bu.edu>
2018-03-27 09:59:44 -0400

    Updated the 'readin.py' file so that it assigns the molar mass of the atoms given in the 'coord.dat' file.

--- py/test/main.py
+++ py/test/main.py
@@ -1,5 +1,3 @@
 import numpy as np
-import importlib as il
-
 from readin import *
 
--- py/test/readin.py
+++ py/test/readin.py
@@ -1,13 +1,17 @@
 import numpy as np
+from periodic import element
 
 # read in the coordinates for your system
 num_atoms = len(open('coords.dat', 'r').readlines())
 coords = np.zeros((num_atoms, 3))
+atm_mass = np.zeros(num_atoms)
 
 fn = open('coords.dat', 'r')
 for i in range(num_atoms):
     c_info = fn.readline().split()
+    atm = c_info[0]
     coords[i, :] = map(float, c_info[1:4])
+    atm_mass[i] = element(atm).mass
 fn.close()
 
 # read in the excited state forces on your atoms
@@ -15,9 +19,8 @@ num_eforces = len(open('eforces.dat', 'r').readlines())
 eforces = np.zeros((num_eforces, 4))
 
 fn = open('eforces.dat', 'r')
-for i in range(num_eforces):
+for i in range(num_eforces-1):
     e_info = fn.readline().split()
-    print i, e_info[2:5]
     eforces[i, :] = map(float, e_info[1:5])
 fn.close()
 
@@ -26,7 +29,7 @@ num_gforces = len(open('gforces.dat', 'r').readlines())
 gforces = np.zeros((num_gforces, 4))
 
 fn = open('gforces.dat', 'r')
-for i in range(num_gforces):
+for i in range(num_gforces-1):
     g_info = fn.readline().split()
     gforces[i, :] = map(float, g_info[1:5])
 fn.close()
